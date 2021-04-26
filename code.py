8dfe920e8ddc5215b0324aa0ac2766b15b72edec
Raymond <raydiwan@gmail.com>
2021-04-26 18:21:25 -0300

    baralhos

---
+++ code.py
@@ -0,0 +1,5 @@
+#criar baralco
+deck_espadas = ['2e', '3e', '4e', '5e', '6e', '7e', '8e', '9e', '10e', 'Je', 'Qe', 'Ke', 'Ae']
+deck_copas = ['2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', '10c', 'Jc', 'Qc', 'Kc', 'Ac']
+deck_trevos = ['2t', '3t', '4t', '5t', '6t', '7t', '8t', '9t', 'Tt', 'Jt', 'Qt', 'Kt', 'At']
+deck_ouro = ['2o', '3o', '4o', '5o', '6o', '7o', '8o', '9o', '10o', 'Jo', 'Qo', 'Ko', 'Ao']
