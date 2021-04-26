dca9f8e08cdd2af4ca930fddfa3a038af4a7311f
Raymond <raydiwan@gmail.com>
2021-04-26 18:56:51 -0300

    baralho misturado

--- code.py
+++ code.py
@@ -1,17 +1,13 @@
-import random 
-
+import random
 
 #criar baralho
-deck_espadas = ['2e', '3e', '4e', '5e', '6e', '7e', '8e', '9e', '10e', 'Je', 'Qe', 'Ke', 'Ae']
-deck_copas = ['2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', '10c', 'Jc', 'Qc', 'Kc', 'Ac']
-deck_paus = ['2p', '3p', '4p', '5p', '6p', '7p', '8p', '9p', 'pp', 'Jp', 'Qp', 'Kp', 'Ap']
-deck_ouro = ['2o', '3o', '4o', '5o', '6o', '7o', '8o', '9o', '10o', 'Jo', 'Qo', 'Ko', 'Ao']
 
 numeros =  ['2','3', '4' ,'5','6', '7','8','9','10','J','Q','K','A']
 naipes = ['♠','♣',' ♦','♥']
 
+baralho = ['A♥', 'K♥', 'Q♥', 'J♥', '10♥', '9♥', '8♥', '7♥', '6♥', '5♥', '4♥','3♥', '2♥','A♠', 'K♠', 'Q♠', 'J♠', '10♠','9♠', '8♠', '7♠', '6♠', '5♠', '4♠', '3♠', '2♠','A♦','K♦','Q♦', 'J♦', '10♦', '9♦', '8♦', '7♦', '6♦', '5♦', '4♦', '3♦', '2♦', 'A♣', 'K♣', 'Q♣', 'J♣', '10♣', '9♣', '8♣', '7♣', '6♣', '5♣', '4♣', '3♣', '2♣']
 
-baralho = []
-def criar_baralho(numeros,naipes):
-    cartas = 
-    baralho.append()
+
+def cria_baralho():
+    baralho_misturado = random.sample(baralho, len(baralho))
+    return(baralho_misturado)
