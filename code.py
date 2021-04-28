04736974703e9e15f86f0467641783c352ea99cb
Guillermo02 <guigokuznietz@gmail.com>
2021-04-28 12:27:33 -0300

    Loop, enum e baralho

--- code.py
+++ code.py
@@ -1,12 +1,12 @@
 import random
 
+#numeros =  ['2','3', '4' ,'5','6', '7','8','9','10','J','Q','K','A']
+#naipes = ['♠','♣',' ♦','♥']
+
 #criar baralho
-
-numeros =  ['2','3', '4' ,'5','6', '7','8','9','10','J','Q','K','A']
-naipes = ['♠','♣',' ♦','♥']
-
 baralho = ['A♥', 'K♥', 'Q♥', 'J♥', '10♥', '9♥', '8♥', '7♥', '6♥', '5♥', '4♥','3♥', '2♥','A♠', 'K♠', 'Q♠', 'J♠', '10♠','9♠', '8♠', '7♠', '6♠', '5♠', '4♠', '3♠', '2♠','A♦','K♦','Q♦', 'J♦', '10♦', '9♦', '8♦', '7♦', '6♦', '5♦', '4♦', '3♦', '2♦', 'A♣', 'K♣', 'Q♣', 'J♣', '10♣', '9♣', '8♣', '7♣', '6♣', '5♣', '4♣', '3♣', '2♣']
 
+#Mistura o baralho
 baralho_misturado = []
 def cria_baralho():
     baralho_misturado = random.sample(baralho, len(baralho))
@@ -14,6 +14,7 @@ def cria_baralho():
 
 new_baralho = cria_baralho()
 print(new_baralho)
+
 
 #########################################################################################################
 lista_naipes = []
@@ -37,30 +38,48 @@ for i in new_baralho:
 
 print(lista_valores)
 
+for i,value in enumerate(new_baralho, 1):
+    print(i,value)
 
-jog = new_baralho[8]
-i = 8
+carta_jog = input('Escolha uma carta: ')
+i = 50
+
 def lista_movimentos_possiveis(new_baralho):
     sol1 = False
     sol2 = False
-    if len(jog)>2:
-        valor = jog[0] + jog[1]
-        naipe = jog[2]
+    if len(carta_jog)>2:
+        valor = carta_jog[0] + carta_jog[1]
+        naipe = carta_jog[2]
     else:
-        valor = jog[0]
-        naipe = jog[1]
+        valor = carta_jog[0]
+        naipe = carta_jog[1]
     
     #Verifica valor e naipe
-    if lista_valores[i] == lista_valores[i-1] or lista_naipes[i] == lista_naipes[i-1]:
+    if lista_valores[i-1] in carta_jog or lista_naipes[i-1] in carta_jog:
         sol1 = True
-    elif lista_valores[i] == lista_valores[i-3] or lista_naipes[i] == lista_naipes[i-3]:
+    elif lista_valores[i-3] in carta_jog or lista_naipes[i-3] in carta_jog:
         sol2 = True
 
     if sol1 == True and sol2==True:
         return [1, 3]
     elif sol1 == True and sol2==False:
+        del(lista_valores[i-1])
+        del(lista_naipes[i-1])
+        del(new_baralho[i-1])
         return [1]
     elif sol1 == False and sol2==True:
+        del(lista_valores[i-3])
+        del(lista_naipes[i-3])
+        del(new_baralho[i-3])
         return [3]
     else:
         return []
+    print(new_baralho)
+print(lista_movimentos_possiveis(new_baralho))
+
+for i,value in enumerate(new_baralho, 1):
+    print(i,value)
+    
+while carta_jog != 'n':
+    lista_movimentos_possiveis(new_baralho)
+    carta_jog = input('Escolha uma carta: ')
